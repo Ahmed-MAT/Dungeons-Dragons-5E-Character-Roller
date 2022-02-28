f0422c5bd1c14126186bbadc7ae5b7c64f9f2d68
Ahmed-MAT <84765687+Ahmed-MAT@users.noreply.github.com>
2022-02-27 23:17:00 -0800

    Multi-Character Generation added
    - Added the ability to generate characters in batches, instead of one at a time.
    - Edited for-loops for greater indentation clarity.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 
-def get_input(prompt = "??"):
+def get_input(prompt="??"):
     result = input(prompt)
     return result
 
-def get_int(prompt = "??"):
+def get_int(prompt="??"):
     result = get_input(prompt)
     while result:
         try:
@@ -16,47 +16,55 @@ def get_int(prompt = "??"):
             result = int(result)
             return result
 
-def generate_character(x = None):
+def generate_character(number_of_characters=1, x=None):
+    number_of_characters = int(number_of_characters)
     while not x == 3 and not x == 4:
         x = get_int("How many d6's do you want to roll? 3 or 4? ")
 
     rolls = []
     sets = []
 
-    if x == 3:
-        for i in range(3):
-            for i in range(6):
-                total = 0
-                for e in range(3):
-                    roll = random.randint(1, 6)
-                    if roll == 1:
+    for character in range(number_of_characters):
+        if x == 3:
+            for i in range(3):
+                for e in range(6):
+                    total = 0
+                    for z in range(3):
                         roll = random.randint(1, 6)
-                    total = total + roll
-                rolls.append(total)
-            sets.append(rolls)
-            rolls = []
-        return sets
-    if x == 4:
-        for i in range(3):
-            for i in range(6):
-                total = 0
-                elimination = []
-                for e in range(4):
-                    roll = random.randint(1, 6)
-                    if roll == 1:
+                        if roll == 1:
+                            roll = random.randint(1, 6)
+                        total = total + roll
+                    rolls.append(total)
+                sets.append(rolls)
+                rolls = []
+            print(f"{sets}")
+            sets = []
+        if x == 4:
+            for i in range(3):
+                for e in range(6):
+                    total = 0
+                    elimination = []
+                    for z in range(4):
                         roll = random.randint(1, 6)
-                    elimination.append(roll)
-                elimination.sort()
-                elimination.pop(0)
-                for i in range(len(elimination)):
-                    total = total + elimination[i]
-                rolls.append(total)
-            sets.append(rolls)
-            rolls = []
-        return sets
+                        if roll == 1:
+                            roll = random.randint(1, 6)
+                        elimination.append(roll)
+                    elimination.sort()
+                    elimination.pop(0)
+                    for z in range(len(elimination)):
+                        total = total + elimination[z]
+                    rolls.append(total)
+                sets.append(rolls)
+                rolls = []
+            print(f"{sets}")
+            sets = []
 
-def generate_characters():
-    print(generate_character())
+    complete = "Character generation complete."
+    return complete
+
+def generate_characters(number_of_characters=None):
+    number_of_characters = get_int("How many characters would you like to generate? ")
+    print(generate_character(number_of_characters))
     go_again = get_int("Do you want to generate another? 1 = Yes, 2 = No ")
     valid_inputs = [1, 2]
 
@@ -65,7 +73,7 @@ def generate_characters():
         go_again = get_int("Do you want to generate another? 1 = Yes, 2 = No ")
 
     while go_again == 1:
-        print(generate_character())
+        print(generate_character(number_of_characters))
         go_again = get_int("Do you want to generate another? 1 = Yes, 2 = No ")
 
     if go_again == 2:
